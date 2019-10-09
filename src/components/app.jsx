fe2a175860af07c8171cb0a23dd99fa2154271b2
Christine Schorn <cschorn91@gmail.com>
2019-10-09 16:48:08 +1100

    created gif_list component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
 import SearchBar from './search_bar';
 import Gif from './gif';
+import GifList from './gif_list';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: '4Zo41lhzKt6iZ8xff9'
+    }
+  }
+
   render() {
+    const gifs = [
+      { id: '4Zo41lhzKt6iZ8xff9' },
+      { id: '51Uiuy5QBZNkoF3b2Z' }
+    ];
+
     return (
       <div>
         <div className="left-scene">
           <SearchBar />
           <div className="selected-gif">
-            <Gif id = '4Zo41lhzKt6iZ8xff9'/>
+            <Gif id = { this.state.selectedGifId }/>
           </div>
         </div>
-        <div className="right-scene"></div>
+        <div className="right-scene">
+          <GifList gifs = { gifs } />
+        </div>
       </div>
     );
   }
