498bcd6773ad8114fdbdf0d5c16fe95fcbef539f
Christine Schorn <cschorn91@gmail.com>
2019-10-10 16:22:19 +1100

    added feature to exchange main gif for selected gif from giflist

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 
 class Gif extends Component {
+  handlOnClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
   render() {
     const src = `https://media1.giphy.com/media/${this.props.id}/giphy.webp?cid=790b761178c835cbf9d629d2b5c082189fc1bae2c3bb337f&rid=giphy.webp`;
    
     return (
-      <img src = { src } alt = "" className = "gif"/>
+      <img src = { src } alt = "" className = "gif"
+      onClick = { this.handlOnClick } />
     ); 
   }
 }
